e3905fe15f9fd640b6078525af877c898de33749
Alex <158052515+WeeakLink@users.noreply.github.com>
2025-03-08 23:26:33 +1100

    buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -1,43 +1,7 @@
-//jQuery shortcut for document.onload()
-$(function(){
-    initButtonListeners()
-});
-
-// Setup the onclick event listeners for the buttons
-function initButtonListeners() {
-
-    // find all objects with button class
-    var $buttons = $('.button');
-
-    // loop over each button, accessed using the variable 'this'
-    $buttons.each(function() {
-
-        // create the function that runs when this element is clicked on.
-        $(this).click(function () {
-
-            // toggle button
-            toggleButton($(this))
-            
-            // show/hide the text in the next sibling with 500ms animation
-            $(this).next().toggle(500);
-          });
+$(document).ready(function(){
+    $('.button').on('click', function() {
+        $(this).toggleClass('down up')
+               .css('background-image', $(this).hasClass('up') ? 'url(img/up_arrow50.png)' : 'url(img/down_arrow50.png)');
+        $(this).next().slideToggle(500);
     });
-}
-
-/*
- Show an up or down arrow depending on current state
- Calling this will toggle up/down state
-
- @param button jQuery object containing a button
-*/
-function toggleButton(button) {
-    if (button.hasClass('down')) {
-        button.css('background-image', 'url(img/up_arrow50.png)');
-        button.removeClass('down');
-        button.addClass('up');
-    } else if (button.hasClass('up')) {
-        button.css('background-image', 'url(img/down_arrow50.png)');
-        button.removeClass('up');
-        button.addClass('down');
-    }
-}
+});
